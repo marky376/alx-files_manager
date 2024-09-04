f9f2dc529406d4f0282f3a7bfe5c0939a79cfa0e
marky376 <mmutuamark@gmail.com>
2024-09-04 18:06:14 +0300

    The index.js file for my first API

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 import FilesController from '../controllers/FilesController';
-//import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-//import { APIError, errorResponse } from '../middlewares/error';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+import { APIError, errorResponse } from '../middlewares/error';
 
 /**
  * Injects routes with their handlers to the given Express application.
