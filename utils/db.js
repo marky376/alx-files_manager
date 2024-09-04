886fe12076b8c3fb8fea90ac7138f3d9526780e5
marky376 <mmutuamark@gmail.com>
2024-09-04 10:30:35 +0300

    And so my First API next

--- utils/db.js
+++ utils/db.js
@@ -4,63 +4,62 @@ import Collection from 'mongodb/lib/collection';
 import envLoader from './env_loader';
 
 /**
- * Represents a database connection.
+ * Represents a MongoDB client.
  */
-
 class DBClient {
-    /**
-     * Creates a new DBClient instance.
-     */
-    constructor() {
-        envLoader();
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const dbURL = `mongodb://${host}:${port}/$database}`;
+  /**
+   * Creates a new DBClient instance.
+   */
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
 
-        this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true});
-        this.client.connect();
-    }
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
 
+  /**
+   * Checks if this client's connection to the MongoDB server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-    /**
-     * Checks if this client's connection to the MongoDB server is active.
-     * @returns {boolean}
-     */
+  /**
+   * Retrieves the number of users in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
 
-    isAlive() {
-        return this.client.isConnected();
-    }
+  /**
+   * Retrieves the number of files in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
 
-    /**
-     * Retrieves the number of users in he dtabase.
-     * @returns {Promise<Number>}
-     */
+  /**
+   * Retrieves a reference to the `users` collection.
+   * @returns {Promise<Collection>}
+   */
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
 
-    async nbUsers() {
-        return this.client.db().collection('users').countDocuments();
-    }
-
-    /**
-     * Retrieves the number of files in the database.
-     * @returns {Promise<Number>}
-     */
-    async nbFiles() {
-        return this.client.db().collection('files').countDocuments();
-
-    }
-
-    /**
-     * Retrieves a reference to the 'users' collection.
-     * @returns {Promise<Collection>}
-     */
-    async usersCollection() {
-        return this.client.db().collection('files');
-    }
-
-    async filesCollection() {
-        return this.client.db().collection('files');
-    }
+  /**
+   * Retrieves a reference to the `files` collection.
+   * @returns {Promise<Collection>}
+   */
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
 }
 
 export const dbClient = new DBClient();
