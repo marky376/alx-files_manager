83ea5832ab67ccb407bc010477dc39c4a802eae8
marky376 <mmutuamark@gmail.com>
2024-09-04 09:00:25 +0300

    Working on the DbClient class

--- utils/db.js
+++ utils/db.js
@@ -21,4 +21,47 @@ class DBClient {
         this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true});
         this.client.connect();
     }
+
+
+    /**
+     * Checks if this client's connection to the MongoDB server is active.
+     * @returns {boolean}
+     */
+
+    isAlive() {
+        return this.client.IsConnected();
+    }
+
+    /**
+     * Retrieves the number of users in he dtabase.
+     * @returns {Promise<Number>}
+     */
+
+    async nbUsers() {
+        return this.client.db().collection('users').countDocuments();
+    }
+
+    /**
+     * Retrieves the number of files in the database.
+     * @returns {Promise<Number>}
+     */
+    async nbFiles() {
+        return this.client.db().collection('files').countDocuments();
+
+    }
+
+    /**
+     * Retrieves a reference to the 'users' collection.
+     * @returns {Promise<Collection>}
+     */
+    async usersCollection() {
+        return this.client.db().collection('files');
+    }
+
+    async filesCollection() {
+        return this.client.db().collection('files');
+    }
 }
+
+export const dbClient = new DBClient();
+export default dbClient;
